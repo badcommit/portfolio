7ad4dd955ff63292e26b332fb312ffab436e1d4f
apple zhang <zhangshe@usc.edu>
2023-07-20 20:41:48 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 
 import NewsletterForm from '@/components/NewsletterForm'
-import { AnimatedBy } from './animation/animation'
+import { AnimatedBy } from '../components/animation/animation'
 import Bg from './welcome'
 
 const MAX_DISPLAY = 5
